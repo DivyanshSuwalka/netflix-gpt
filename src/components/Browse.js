23a86ccdd8cacf632d85a2215eceee51f1c768aa
DivyanshSuwalka <suwalkadivyansh63@gmail.com>
2025-09-25 13:27:02 +0530

    auth-firebase

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,5 +1,36 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+
 const Browse = () => {
-  return <div>Browse</div>;
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return (
+    <div className="flex justify-between bg-gradient-to-b from-black bg-opacity-10">
+      <img
+        className="w-40 my-6 mx-12"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+      <button
+        onClick={handleSignOut}
+        className="my-10 m-16 px-2 bg-red-700 rounded-md text-white font-bold"
+      >
+        Sign Out
+      </button>
+    </div>
+  );
 };
 
 export default Browse;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,79 @@
+import { checkValidData } from "../utils/validate";
 import Header from "./Header";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
+  const navigate = useNavigate();
 
-  function toggleSignInForm() {
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const fName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const toggleSignInForm = () => {
     setIsSignIn(!isSignIn);
-  }
+  };
+  const handleButtonClick = () => {
+    // console.log(email.current.value, password.current.value);
+    // validating form data
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
+
+    if (!isSignIn) {
+      // signup logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: fName.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/76103075?v=4",
+          })
+            .then(() => {
+              // Profile updated!
+              navigate("/browse");
+              console.log(user, auth);
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.code);
+            });
+        })
+        .catch((error) => {
+          console.log(error.code);
+          setErrorMessage(error.code);
+        });
+    } else {
+      // signin logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          // const user = userCredential.user;
+          navigate("/browse");
+        })
+        .catch((error) => {
+          console.log(error.code);
+          setErrorMessage(error.code);
+        });
+    }
+  };
 
   return (
     <div>
@@ -17,28 +84,40 @@ const Login = () => {
           alt="bg"
         />
       </div>
-      <form className="absolute my-36 mx-auto right-0 left-0 w-2/6 p-12 bg-black bg-opacity-75 rounded-md">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="absolute my-36 mx-auto right-0 left-0 w-2/6 p-12 bg-black bg-opacity-75 rounded-md"
+      >
         <h1 className="m-2 text-3xl text-white font-bold mb-4">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignIn && (
           <input
+            ref={fName}
             type="text"
             placeholder="Full Name"
-            className="p-4 my-2 bg-opacity-30 rounded-md bg-gray-700 w-full"
+            className="p-4 my-2 bg-opacity-30 rounded-md text-white bg-gray-700 w-full"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email or mobile number"
-          className="p-4 my-2 bg-opacity-30 rounded-md bg-gray-700 w-full"
+          className="p-4 my-2 bg-opacity-30 rounded-md text-white bg-gray-700 w-full"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
-          className="p-4 my-2 bg-opacity-30 rounded-md bg-gray-700 w-full"
+          className="p-4 my-2 bg-opacity-30 rounded-md text-white bg-gray-700 w-full"
         />
-        <button className="py-2 px-4 my-2 w-full bg-red-700 rounded-md text-white font-bold">
+        <p className="text-red-500">{errorMessage}</p>
+        <button
+          onClick={handleButtonClick}
+          className="py-2 px-4 my-2 w-full bg-red-700 rounded-md text-white font-bold"
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <p
