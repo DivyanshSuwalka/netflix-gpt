f522bef916285db742c7c8874bfdab73293783a0
DivyanshSuwalka <suwalkadivyansh63@gmail.com>
2025-10-02 12:27:50 +0530

    Build mainContainer & TMDB API data Fetch

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -5,11 +5,10 @@ const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
   return (
-    <div className="w-full h-[400px] sm:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px]">
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key+"?autoplay=1&mute=1&controls=0&loop=1&playlist="+trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,13 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt space-y-4 p-8 max-w-lg">
-      <h1 className="text-6xl">{title}</h1>
-      <p>{overview}</p>
+    <div className="pt-[20%] pl-[5%] absolute w-screen aspect-video text-white bg-gradient-to-r from-black space-y-4">
+      <h1 className="text-6xl w-1/2 line-clamp-2">{title}</h1>
+      <p className="w-1/3">{overview}</p>
       <div className="space-x-4">
-        <button className="p-2 px-8 text-lg hover:shadow-xl bg-gray-300 rounded-md ">
+        <button className="p-2 px-8 text-lg hover:shadow-xl bg-gray-300 text-black rounded-md hover:bg-opacity-50">
           	Play
         </button>
-        <button className="p-2 px-8 text-lg hover:shadow-xl bg-gray-300 rounded-md ">
+        <button className="p-2 px-8 text-lg hover:shadow-xl bg-gray-300 bg-opacity-10 rounded-md ">
           More Info
         </button>
       </div>
